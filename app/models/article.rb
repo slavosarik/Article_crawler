0d53a4a0b478c3f6d2119c0e52e4d25a7f24e1b1
slavosarik <slavosarik@gmail.com>
2014-07-08 21:52:49 +0200

    parse function created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,16 @@
 class Article < ActiveRecord::Base
+
+    def self.parse_from_aktuality(html)
+      doc = Nokogiri::HTML.parse(html)
+
+      doc.search('.item-top .col-text').map do |div|
+        anchor = div.search('h2 a').first
+        Article.create!(
+            id_article: anchor[:href][/\/(\d+)\//, 1].to_i,
+            title: anchor.text,
+            perex: div.search('p').text
+        )
+      end
+    end
+
 end
