2bcfde99deb0a0d968eaebc6b939b905bb467317
slavosarik <slavosarik@gmail.com>
2014-07-08 21:40:27 +0200

    article model schema - added null restriction to columns

--- db/migrate/20140708104958_create_articles.rb
+++ db/migrate/20140708104958_create_articles.rb
@@ -1,11 +1,12 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.integer :id_article
-      t.string :title
-      t.text :perex
+      t.integer :id_article, null: false
+      t.string :title, null: false
+      t.text :perex, null: false
 
       t.timestamps
+
     end
   end
 end
