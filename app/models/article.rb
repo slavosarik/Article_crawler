8694de3992cea7102914cee170950e17dff5f3b6
slavosarik <slavosarik@gmail.com>
2014-07-09 00:18:04 +0200

    parse function - fixed error (missing dot in parse command)

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,35 +3,18 @@ class Article < ActiveRecord::Base
     def self.parse_from_aktuality(html)
       doc = Nokogiri::HTML.parse(html)
 
+      #puts doc
       #TODO: upravit parsovanie podla HTML tagov
 
-      doc.search('article-info-bottom').map do |div|
+      doc.search('.article-info-bottom').map do |div|
         anchor = div.search('a').first
-        puts anchor
+
         Article.create!(
             id_article: anchor[:href][/\/(\d+)\//, 1].to_i,
-            title: anchor.search('span').first.text,
-            perex: div.search('article-introtext').text
+            title: anchor.search('.article-title').first.text,
+            perex: div.search('.article-introtext').text
         )
       end
     end
 
 end
-
-
-#class Article < ActiveRecord::Base
-#  belongs_to :author
-#
-#  def self.parse_from_sme(html)
-#    doc = Nokogiri::HTML.parse(html)
-#
-#    doc.search('.item-top .col-text').map do |div|
-#      anchor = div.search('h2 a').first
-#      Article.create!(
-#          sme_id: anchor[:href][/\/(\d+)\//, 1].to_i,
-#          title: anchor.text,
-#          perex: div.search('p').text
-#      )
-#    end
-#  end
-#end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe Article, :type => :model do
 
     Article.parse_from_aktuality(html)
 
-    #expect(Article.count).not_to eq(0)
+    expect(Article.count).not_to eq(0)
 
     article = Article.first
-    puts "ahahaha"
-
     expect(article.title).to eq('Fico ustúpil opozícii, vývoz vody chce stopnúť v Ústave')
     #expect(article.perex).to eq('Opozícia dostala ponuku hlasovať za ďalšiu zmenu Ústavy.')
     #expect(article.sme_id).to eq(7273950)
