7c718f4cad61a1a9971a4c0c8437d2fca2a20309
slavosarik <slavosarik@gmail.com>
2014-07-08 22:43:00 +0200

    parse function edit

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
 
       #TODO: upravit parsovanie podla HTML tagov
 
-      doc.search('.item-top .col-text').map do |div|
+      doc.search('.article-box article-box-big article-box-section-1 .col-text').map do |div|
         anchor = div.search('h2 a').first
         Article.create!(
             id_article: anchor[:href][/\/(\d+)\//, 1].to_i,
