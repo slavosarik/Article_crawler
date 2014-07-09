f94cb267ab81160d81e3ad8bae16439c8d9fd271
slavosarik <slavosarik@gmail.com>
2014-07-09 10:02:30 +0200

    reset

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ActiveRecord::Base
         anchor = div.search('a').first
         Article.create!(
             id_article: anchor[:href][/\/(\d+)\//, 1].to_i,
-            title: anchor.search('.article-title').first.text.gsub(/[[:space:]]/, ' ').gsub(/\s{2,}/, ' '),
+            title: anchor.search('.article-title').first.text.gsub(/[\s]+/,' ').strip,      #gsub(/[\t,\n]/, ''),
             perex: div.search('.article-introtext').text
         )
       end
