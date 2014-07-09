f7255c339441324e74335dd94602136618b7f6ca
slavosarik <slavosarik@gmail.com>
2014-07-09 10:50:55 +0200

    parse final fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,16 @@
 class Article < ActiveRecord::Base
 
-    def self.parse_from_aktuality(html)
+    def self.parse_from_sportky(html)
       doc = Nokogiri::HTML.parse(html)
 
-      doc.search('.article-info-bottom').map do |div|
+      doc.search('article').map do |div|
+
         anchor = div.search('a').first
-        Article.create!(
+
+         Article.create!(
             id_article: anchor[:href][/\/(\d+)\//, 1].to_i,
-            title: anchor.search('.article-title').first.text.gsub(/[\s]+/,' ').strip,      #gsub(/[\t,\n]/, ''),
-            perex: div.search('.article-introtext').text
+            title: div.search('h2 a').text.gsub(/[\s]+/, ' ').strip,
+            perex: div.search('p').text.gsub(/[\s]+/, ' ').strip
         )
       end
     end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
   it 'parses articles' do
-    html = File.read(File.dirname(__FILE__) + '/fixtures/football.html', encoding: 'UTF-8')
+    html = File.read(File.dirname(__FILE__) + '/fixtures/futbal.html', encoding: 'UTF-8')
 
-    Article.parse_from_aktuality(html)
+    #html = Faraday.get 'http://sportky.topky.sk/s/13/futbal'
+
+
+    Article.parse_from_sportky(html.body)
 
     expect(Article.count).not_to eq(0)
 
     article = Article.first
 
-    expect(article.title).to eq('Brazílsky obranca Dodo posilnil Inter Miláno')
-    expect(article.perex).to eq('Futbalisti AS Trenčín zdolali v poslednom prípravnom stretnutí pred štartom najvyššej slovenskej súťaže Slaviu Praha 1:0.')
-    expect(article.id_article).to eq(170733)
+    expect(article.title).to eq('Brazílčania opisujú debakel ako historickú hanbu: Nemecko urobilo z Brazílie mleté ​​mäso')
+    expect(article.perex).to eq('RIO DE JANEIRO - Brazílska tlač je rovnako ako všetci fanúšikovia "kanárikov" zdrvená debaklom 1:7, ktorý domácim futbalistom uštedrili nemeckí reprezentanti v semifinále ...')
+    expect(article.id_article).to eq(126863)
+
+    puts "koniec"
+
   end
 end
